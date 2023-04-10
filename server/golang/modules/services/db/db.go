f70a0950391e6c0aec1483587e11bce0647cda54
Alexander Shcherbakov <alexander.sh.sa@gmail.com>
2023-04-10 20:46:35 +0200

    feat(go): add pg connection

---
+++ server/golang/modules/services/db/db.go
@@ -0,0 +1,13 @@
+package db
+
+import (
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "your-password"
+	dbname   = "calhounio_demo"
+)
